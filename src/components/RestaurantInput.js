ac5c6c54ddb7ee2d50a0ec71cb87a5114dc6bf0c
merewelch214 <merewelch@gmail.com>
2020-03-06 10:00:39 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -49,6 +50,17 @@ export class RestaurantInput extends Component {
   }
 };
 
+const msp = state => {
+  return { restaurants: state.restaurants }
+}
+
+const mdp = dispatch => {
+  return {
+    addRestaurant: (rest) => {
+      dispatch(addRestaurant(rest))
+    }
+  }
+}
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(msp, mdp)(RestaurantInput)
